6d302991903c9a0297265c6ce8f72e4a9893c8a9
Ekaterina Grigorenko <katrinvesna13@gmail.com>
2022-05-23 22:08:57 +0700

    HW iosui-3 added

---
+++ iosui-3/Navigation/Navigation/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Екатерина Александренко on 20.05.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
+
---
+++ iosui-3/Navigation/Navigation/InfoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  InfoViewController.swift
+//  Navigation
+//
+//  Created by Екатерина Александренко on 23.05.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        self.navigationItem.title = "Информация"
+        makeButton()
+    }
+    
+    private func makeButton() {
+        let alertButton = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 40))
+        alertButton.center = view.center
+        alertButton.setTitle("Внести изменения", for: .normal)
+        alertButton.backgroundColor = .systemBlue
+        alertButton.layer.cornerRadius = 12
+        alertButton.addTarget(self, action: #selector(tapAlertAction), for: .touchUpInside)
+        view.addSubview(alertButton)
+    }
+    
+    @objc private func tapAlertAction () {
+        let alert = UIAlertController(title: "Внесены изменения", message: "Сохранить?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Да", style: .default) {_ in
+            print("Изменения сохранены")
+            // self.dismiss(animated: true)
+            self.navigationController?.popViewController(animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .destructive) { _ in
+            print("Отмена")
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: false)
+        
+    }
+}
